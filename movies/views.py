41c5ce70276aa6cfa30af356d85d8d32b7f7d281
cadavis7261yt <cadavis7261yt@gmail.com>
2019-06-21 02:59:18 +0000

    Created django movie web app

--- movies/views.py
+++ movies/views.py
@@ -20,7 +20,7 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url') or 'https://www.classicposters.com/images/nopicture.gif'}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://content.schoolinsites.com/api/documents/ebbca81b01694c91aa908f5374842a9f.gif'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
@@ -37,7 +37,7 @@ def edit(request, movie_id):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url') or 'https://www.classicposters.com/images/nopicture.gif'}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://content.schoolinsites.com/api/documents/ebbca81b01694c91aa908f5374842a9f.gif'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
